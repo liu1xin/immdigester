ffc580fb6910da94054d96c5cfab25ecf194a425
liuyx <liu1xin@outlook.com>
2015-08-26 20:01:45 +0800

    建立项目基本目录

---
+++ src/immscrapy/items.py
@@ -0,0 +1,5 @@
+'''
+Created on 2015年8月26日
+
+@author: liu1xin@outlook.com
+'''
---
+++ src/immscrapy/spiders/indexdocSpider/indexdocSpider.py
@@ -0,0 +1,16 @@
+'''
+Created on 2015年8月26日
+
+@author: liu1xin@outlook.com
+'''
+
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
